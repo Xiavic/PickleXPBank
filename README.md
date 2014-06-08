d1833056c2a1cd59ac07b20ad0abfba34fcf4467
Pickle15 <dill16pickle1993@gmail.com>
2014-06-08 01:03:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+PickleXPBank
+============
+
+This Bukkit plugin allows players to store XP points, not levels, into signs.
---
+++ src/main/java/net/picklecraft/picklexpbank/Account.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 Pickle <curtisdhi@gmail.com>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package net.picklecraft.picklexpbank;
+
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Pickle <curtisdhi@gmail.com>
+ */
+public class Account {
+    
+    private final Player player;
+    private long balance;
+    
+    public Account(Player player, long balance) {
+        this.player = player;
+        this.balance = balance;
+    }
+    public Account(Player player) {
+        this(player, 0);
+    }
+    
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+    public long getBalance() {
+        return balance;
+    }
+    
+    public void addBalance(long amount) {
+        balance += amount;
+    }
+    public void subBalace(long amount) {
+        balance -= amount;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+    
+}
